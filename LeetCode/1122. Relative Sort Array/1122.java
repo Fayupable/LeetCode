dddac21b76e1f980e30d69b00fde85cd91587456
Fayupable <90789180+Fayupable@users.noreply.github.com>
2024-06-11 19:32:30 +0300

    update

--- LeetCode/1122. Relative Sort Array/1122.java
+++ LeetCode/1122. Relative Sort Array/1122.java
@@ -88,3 +88,111 @@ class Solution {
     }
 }
  */
+/*
+ import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        Map<Integer, Integer> rank = new HashMap<>();
+        for (int i = 0; i < arr2.length; i++) {
+            rank.put(arr2[i], i);
+        }
+
+        Integer[] arr1Wrapper = new Integer[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1Wrapper[i] = arr1[i];
+        }
+
+        mergeSort(arr1Wrapper, rank);
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = arr1Wrapper[i];
+        }
+
+        return arr1;
+    }
+
+    private void mergeSort(Integer[] arr, Map<Integer, Integer> rank) {
+        if (arr.length < 2) return;
+        int mid = arr.length / 2;
+        Integer[] left = new Integer[mid];
+        Integer[] right = new Integer[arr.length - mid];
+
+        System.arraycopy(arr, 0, left, 0, mid);
+        System.arraycopy(arr, mid, right, 0, arr.length - mid);
+
+        mergeSort(left, rank);
+        mergeSort(right, rank);
+        merge(arr, left, right, rank);
+    }
+
+    private void merge(Integer[] arr, Integer[] left, Integer[] right, Map<Integer, Integer> rank) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (compare(left[i], right[j], rank) <= 0) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
+
+    private int compare(Integer x, Integer y, Map<Integer, Integer> rank) {
+        if (rank.containsKey(x) && rank.containsKey(y)) {
+            return rank.get(x) - rank.get(y);
+        } else if (rank.containsKey(x)) {
+            return -1;
+        } else if (rank.containsKey(y)) {
+            return 1;
+        } else {
+            return x - y;
+        }
+    }
+}
+
+ */
+/*
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        Map<Integer, Integer> rank = new HashMap<>();
+        for (int i = 0; i < arr2.length; i++) {
+            rank.put(arr2[i], i);
+        }
+
+        for (int i = 1; i < arr1.length; i++) {
+            int key = arr1[i];
+            int j = i - 1;
+            while (j >= 0 && compare(arr1[j], key, rank) > 0) {
+                arr1[j + 1] = arr1[j];
+                j--;
+            }
+            arr1[j + 1] = key;
+        }
+
+        return arr1;
+    }
+
+    private int compare(int x, int y, Map<Integer, Integer> rank) {
+        if (rank.containsKey(x) && rank.containsKey(y)) {
+            return rank.get(x) - rank.get(y);
+        } else if (rank.containsKey(x)) {
+            return -1;
+        } else if (rank.containsKey(y)) {
+            return 1;
+        } else {
+            return x - y;
+        }
+    }
+}
+
+    */
