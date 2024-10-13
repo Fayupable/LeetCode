9529aee8007244cfafb48432a069fba115a408c1
Fayupable <90789180+Fayupable@users.noreply.github.com>
2024-10-13 19:42:35 +0300

    Create 516.java

---
+++ LeetCode/516. Longest Palindromic Subsequence/516.java
@@ -0,0 +1,37 @@
+class Solution {
+    private int[][] dp;
+
+    public int longestPalindromeSubseq(String s) {
+        String reversed = new StringBuilder(s).reverse().toString();
+        
+        int n = s.length();
+        dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        
+        return findCommon(0, 0, s, reversed);
+    }
+
+    public int findCommon(int index1, int index2, String word1, String word2) {
+        if (index1 >= word1.length() || index2 >= word2.length()) {
+            return 0;
+        }
+
+        if (dp[index1][index2] != -1) {
+            return dp[index1][index2];
+        }
+
+        if (word1.charAt(index1) == word2.charAt(index2)) {
+            return dp[index1][index2] = 1 + findCommon(index1 + 1, index2 + 1, word1, word2);
+        }
+
+        return dp[index1][index2] = Math.max(
+            findCommon(index1, index2 + 1, word1, word2),
+            findCommon(index1 + 1, index2, word1, word2)
+        );
+    }
+}
